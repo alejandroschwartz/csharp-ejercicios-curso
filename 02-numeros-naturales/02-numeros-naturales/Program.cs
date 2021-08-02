5cdd3470ddd063157e848fd5192cd9b3e2a623d4
AlejandroSchwartz <alejandromatiassch@gmail.com>
2021-08-02 12:27:20 -0300

    Ejercicio 2 completo y empezando el 3

---
+++ 02-numeros-naturales/02-numeros-naturales/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _02_numeros_naturales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa el primer número");
+            string primero = Console.ReadLine();
+            int uno = Int16.Parse(primero);
+
+            Console.WriteLine("Ingresa el segundo número");
+            string segundo = Console.ReadLine();
+            int dos = Int16.Parse(segundo);
+
+            Console.WriteLine($"Valores ingresados: {primero} y {segundo}");
+
+            int contador = 0;
+            int contadorPares = 0;
+            if (uno > dos)
+            {
+                Console.WriteLine($"El mayor es {uno}");
+                while(dos <= uno)
+                {
+                    Console.WriteLine(dos);
+                    dos++;
+                    contador++;
+                    if(contador % 2 == 0)
+                    {
+                        contadorPares++;
+                    }
+                }
+            }
+            else if (uno < dos)
+            {
+                Console.WriteLine($"El mayor es {dos}");
+                while (dos >= uno)
+                {
+                    Console.WriteLine(uno);
+                    uno++;
+                    contador++;
+                    if (contador % 2 == 0)
+                    {
+                        contadorPares++;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Ambos son iguales a {uno}");
+            }
+
+            Console.WriteLine($"La cantidad de numeros es {contador}");
+            Console.WriteLine($"La cantidad de numeros pares es {contadorPares}");
+        }
+    }
+}
---
+++ 03-vocales-en-una-frase/03-vocales-en-una-frase/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03_vocales_en_una_frase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa texto");
+            String frase = Console.ReadLine();
+
+            int largo = frase.Length;
+            if(largo < 20)
+            {
+                Console.WriteLine($"La cantidad de caracteres es: {largo}");
+
+                //char[] miCaracter = { 'a', 'e', 'i', 'o', 'u' };
+                //string fraseCortada = frase.TrimEnd(miCaracter);
+                //Console.WriteLine(fraseCortada);
+
+                Console.WriteLine(frase.Trim(new Char[] { 'a', 'e', 'i', 'o', 'u' }));
+            } 
+            else
+            {
+                Console.WriteLine("Debe contener menos de 20 caracteres");
+            }
+        }
+    }
+}
