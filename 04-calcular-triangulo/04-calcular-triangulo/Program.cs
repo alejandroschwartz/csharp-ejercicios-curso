27f54c33639e464964235c2c72c92d381c85aa99
AlejandroSchwartz <alejandromatiassch@gmail.com>
2021-08-02 19:55:01 -0300

    Realizando los ejercicios 4 y 5 del curso.

---
+++ 04-calcular-triangulo/04-calcular-triangulo/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace _04_calcular_triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa el lado 1");
+            string lado1 = Console.ReadLine();
+            int uno = Int16.Parse(lado1);
+
+            Console.WriteLine("Ingresa el lado 2");
+            string lado2 = Console.ReadLine();
+            int dos = Int16.Parse(lado2);
+
+            Console.WriteLine("Ingresa el lado 3");
+            string lado3 = Console.ReadLine();
+            int tres = Int16.Parse(lado3);
+
+            Console.WriteLine("--------------------");
+
+            int mayor = ladoMayor(uno, dos, tres);
+            Console.WriteLine($"El mayor es {mayor}");
+
+            Console.WriteLine("--------------------");
+            tipoTriangulo(uno, dos, tres);
+
+        }
+
+        public static void tipoTriangulo(int uno, int dos, int tres)
+        {
+            if (uno == dos && uno == tres)
+            {
+                Console.WriteLine("Es Equilatero");
+            }
+            else if (uno == dos || uno == tres || dos == tres)
+            {
+                Console.WriteLine("Es Isosceles");
+            }
+            else
+            {
+                Console.WriteLine("No es Equilatero ni Isosceles");
+            }
+        }
+
+        public static int ladoMayor(int uno, int dos, int tres)
+        {
+            int mayor = 0;
+
+            if (uno > dos)
+            {
+                if (uno > tres)
+                {
+                    mayor = uno;
+                    if (uno < (dos + tres) )
+                    {
+                        Console.WriteLine($"Lado 1 es mayor, vale: {uno} y es valido");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalido");
+                    }
+                }
+                else
+                {
+                    mayor = tres;
+                    if (tres < (uno + dos))
+                    {
+                        Console.WriteLine($"Lado 3 es mayor, vale: {tres} y es valido");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalido");
+                    }
+                }
+            }
+            else if (dos > uno)
+            {
+                if (dos > tres)
+                {
+                    mayor = dos;
+                    if (dos < (uno + tres))
+                    {
+                        Console.WriteLine($"Lado 2 es mayor, vale: {dos} y es valido");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalido");
+                    }
+                }
+                else
+                {
+                    mayor = tres;
+                    if (tres < (uno + dos))
+                    {
+                        Console.WriteLine($"Lado 3 es mayor, vale: {tres} y es valido");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalido");
+                    }
+                }
+            }
+            return mayor;
+
+        }
+    }
+}
---
+++ 05-multiplos-3-y-2/05-multiplos-3-y-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05_multiplos_3_y_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Contador de multiplos de dos y tres, entre 0 y 100.");
+            Console.WriteLine("--------------------");
+
+            int contador = 0;
+            while (contador <= 100)
+            {
+                if (contador % 2 == 0 && contador % 3 == 0)
+                {
+                    Console.WriteLine($"{contador} multiplo de dos y tres");
+                }
+                else if(contador % 2 == 0)
+                {
+                    Console.WriteLine($"{contador} multiplo de dos");
+                }
+                else if(contador % 3 == 0)
+                {
+                    Console.WriteLine($"{contador} multiplo de tres");
+                }
+                contador++;
+            }
+        }
+    }
+}
