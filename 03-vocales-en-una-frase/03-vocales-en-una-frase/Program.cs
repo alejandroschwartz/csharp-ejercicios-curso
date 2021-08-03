042b7e4987b4098b8bc27cf5c5962945402316e9
AlejandroSchwartz <alejandromatiassch@gmail.com>
2021-08-03 01:15:46 -0300

    Terminando el ejercicio 3 para contar vocales en un texto

--- 03-vocales-en-una-frase/03-vocales-en-una-frase/Program.cs
+++ 03-vocales-en-una-frase/03-vocales-en-una-frase/Program.cs
@@ -7,22 +7,35 @@ namespace _03_vocales_en_una_frase
         static void Main(string[] args)
         {
             Console.WriteLine("Ingresa texto");
-            String frase = Console.ReadLine();
+            string frase = Console.ReadLine().ToLower();
 
-            int largo = frase.Length;
-            if(largo < 20)
+            int cantidad = frase.Length;
+            Console.WriteLine($"La cantidad de caracteres es: {cantidad}");
+            Console.WriteLine("--------------------");
+            
+            if (cantidad <= 20)
             {
-                Console.WriteLine($"La cantidad de caracteres es: {largo}");
-
                 //char[] miCaracter = { 'a', 'e', 'i', 'o', 'u' };
                 //string fraseCortada = frase.TrimEnd(miCaracter);
                 //Console.WriteLine(fraseCortada);
 
-                Console.WriteLine(frase.Trim(new Char[] { 'a', 'e', 'i', 'o', 'u' }));
-            } 
+                //Console.WriteLine(frase.Trim(new Char[] { 'a', 'e', 'i', 'o', 'u' }));
+
+                //frase = frase.Replace("a", "");
+                //frase = frase.Replace("e", "");
+                //frase = frase.Replace("i", "");
+                //frase = frase.Replace("o", "");
+                //frase = frase.Replace("u", "");
+
+                frase = frase.Replace("a", "").Replace("e", "").Replace("i", "").Replace("o", "").Replace("u", "");
+
+                Console.WriteLine($"La cantidad de vocales en {cantidad - frase.Length}");
+                Console.WriteLine("--------------------");
+            }
             else
             {
                 Console.WriteLine("Debe contener menos de 20 caracteres");
+                Console.WriteLine("--------------------");
             }
         }
     }
